13c34a4744fb0843f054ae8a572309dc5088feaf
nixxa <nixxa@mail.ru>
2013-11-08 14:49:03 +0400

    Data

--- MarketGid.UI/Content/maps/mosfilm/junctions.js
+++ MarketGid.UI/Content/maps/mosfilm/junctions.js
@@ -4,22 +4,162 @@ var JunctionData = [
 	{
 		"x": 1497,
 		"y": 774,
-		"name": "Лифт",
+		"name": "Строение 7, этаж 1",
 		"from": "mosfilm-1",
 		"to": "mosfilm-7-1"
 	},
 	{
+		"x": 886,
+		"y": 624,
+		"name": "Строение 2, этаж 3",
+		"from": "mosfilm-1",
+		"to": "mosfilm-2-3"
+	},
+	{
+		"x": 1344,
+		"y": 392,
+		"name": "Строение 3, этаж 1",
+		"from": "mosfilm-1",
+		"to": "mosfilm-3-1"
+	},
+	{
+		"x": 1532,
+		"y": 407,
+		"name": "Строение 27, этаж 1",
+		"from": "mosfilm-1",
+		"to": "mosfilm-27-1"
+	},
+	{
+		"x": 1462,
+		"y": 1025,
+		"name": "Строение 20, этаж 1",
+		"from": "mosfilm-1",
+		"to": "mosfilm-20-1"
+	},
+	{
+		"x": 1859,
+		"y": 1035,
+		"name": "Строение 18, этаж 1",
+		"from": "mosfilm-1",
+		"to": "mosfilm-18-1"
+	},
+	{
+		"x": 1295,
+		"y": 623,
+		"name": "Строение 5, этаж 3",
+		"from": "mosfilm-1",
+		"to": "mosfilm-5-3"
+	},
+	{
+		"x": 1376,
+		"y": 526,
+		"name": "Строение 6, этаж 1",
+		"from": "mosfilm-1",
+		"to": "mosfilm-6-1"
+	},
+	{
+		"x": 897,
+		"y": 2967,
+		"name": "Главный корпус",
+		"from": "mosfilm-2-3",
+		"to": "mosfilm-1"
+	},
+	{
+		"x": 286,
+		"y": 1398,
+		"name": "Главный корпус",
+		"from": "mosfilm-5-3",
+		"to": "mosfilm-1"
+	},
+	{
+		"x": 725,
+		"y": 110,
+		"name": "Главный корпус",
+		"from": "mosfilm-6-1",
+		"to": "mosfilm-1"
+	},
+	{
+		"x": 808,
+		"y": 207,
+		"name": "Этаж 2",
+		"from": "mosfilm-6-1",
+		"to": "mosfilm-6-2"
+	},
+	{
+		"x": 810,
+		"y": 206,
+		"name": "Этаж 1",
+		"from": "mosfilm-6-2",
+		"to": "mosfilm-6-1"
+	},
+	{
+		"x": 94,
+		"y": 268,
+		"name": "Строение 5, этаж 3",
+		"from": "mosfilm-6-2",
+		"to": "mosfilm-5-3"
+	},
+	{
 		"x": 38,
 		"y": 376,
-		"name": "Лифт",
+		"name": "Главный корпус",
 		"from": "mosfilm-7-1",
 		"to": "mosfilm-1"
 	},
 	{
-		"x": 886,
-		"y": 624,
-		"name": "Эскалатор",
-		"from": "mosfilm-1",
-		"to": "mosfilm-2-3"
+		"x": 926,
+		"y": 577,
+		"name": "Этаж 2",
+		"from": "mosfilm-7-1",
+		"to": "mosfilm-7-2"
+	},
+	{
+		"x": 1260,
+		"y": 761,
+		"name": "Этаж 1",
+		"from": "mosfilm-7-2",
+		"to": "mosfilm-7-1"
+	},
+	{
+		"x": 580,
+		"y": 144,
+		"name": "Главный корпус",
+		"from": "mosfilm-18-1",
+		"to": "mosfilm-1"
+	},
+	{
+		"x": 557,
+		"y": 220,
+		"name": "Этаж 2",
+		"from": "mosfilm-18-1",
+		"to": "mosfilm-18-2"
+	},
+	{
+		"x": 504,
+		"y": 220,
+		"name": "Этаж 1",
+		"from": "mosfilm-18-2",
+		"to": "mosfilm-18-1"
+	},
+	{
+		"x": 695,
+		"y": 661,
+		"name": "Главный корпус",
+		"from": "mosfilm-27-1",
+		"to": "mosfilm-1"
+	},
+	{
+		"x": 619,
+		"y": 649,
+		"name": "Этаж 2",
+		"from": "mosfilm-27-1",
+		"to": "mosfilm-27-2"
+	},
+	{
+		"x": 560,
+		"y": 651,
+		"name": "Этаж 1",
+		"from": "mosfilm-27-2",
+		"to": "mosfilm-27-1"
 	}
 ];
