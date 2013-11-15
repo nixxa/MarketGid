03554755a41dd2d2b06e8c6538563ceb3bf68ee5
nixxa <nixxa@mail.ru>
2013-11-15 12:45:54 +0400

    Data

--- MarketGid.UI/Scripts/junctions.js
+++ MarketGid.UI/Scripts/junctions.js
@@ -2,178 +2,165 @@
 
 var JunctionData = [
 	{
-		"x": 1497,
-		"y": 774,
-		"name": "Строение 7, этаж 1",
-		"from": "mosfilm-1",
-		"to": "mosfilm-7-1"
+		"id": 1,
+		"x": 282,
+		"y": 378,
+		"name": "Лифт",
+		"from": "mgu-1",
+		title: "Этаж 1",
+		connectedTo: [15,27]
 	},
 	{
-		"x": 886,
-		"y": 624,
-		"name": "Строение 2, этаж 3",
-		"from": "mosfilm-1",
-		"to": "mosfilm-2-3"
+		"id": 3,
+		"x": 282,
+		"y": 492,
+		"name": "Лифт",
+		"from": "mgu-1",
+		title: "Этаж 1",
+		connectedTo: [13,25]
 	},
 	{
-		"x": 1344,
-		"y": 392,
-		"name": "Строение 3, этаж 1",
-		"from": "mosfilm-1",
-		"to": "mosfilm-3-1"
+		"id": 5,
+		"x": 940,
+		"y": 589,
+		"name": "Лифт",
+		"from": "mgu-1",
+		title: "Этаж 1",
+		connectedTo: [17,29]
 	},
 	{
-		"x": 1532,
-		"y": 407,
-		"name": "Строение 27, этаж 1",
-		"from": "mosfilm-1",
-		"to": "mosfilm-27-1"
+		"id": 7,
+		"x": 1156,
+		"y": 589,
+		"name": "Лифт",
+		"from": "mgu-1",
+		title: "Этаж 1",
+		connectedTo: [19,31]
 	},
 	{
-		"x": 1462,
-		"y": 1025,
-		"name": "Строение 20, этаж 1",
-		"from": "mosfilm-1",
-		"to": "mosfilm-20-1"
+		"id": 9,
+		"x": 940,
+		"y": 958,
+		"name": "Лифт",
+		"from": "mgu-1",
+		title: "Этаж 1",
+		connectedTo: [21,33]
 	},
 	{
-		"x": 1859,
-		"y": 1035,
-		"name": "Строение 18, этаж 1",
-		"from": "mosfilm-1",
-		"to": "mosfilm-18-1"
+		"id": 11,
+		"x": 1156,
+		"y": 958,
+		"name": "Лифт",
+		"from": "mgu-1",
+		title: "Этаж 1",
+		connectedTo: [23,35]
 	},
 	{
-		"x": 1295,
-		"y": 623,
-		"name": "Строение 5, этаж 3",
-		"from": "mosfilm-1",
-		"to": "mosfilm-5-3"
+		"id": 13,
+		"x": 282,
+		"y": 492,
+		"name": "Лифт",
+		"from": "mgu-2",
+		title: "Этаж 2",
+		connectedTo: [3,25]
 	},
 	{
-		"x": 1376,
-		"y": 526,
-		"name": "Строение 6, этаж 1",
-		"from": "mosfilm-1",
-		"to": "mosfilm-6-1"
+		"id": 15,
+		"x": 282,
+		"y": 377,
+		"name": "Лифт",
+		"from": "mgu-2",
+		title: "Этаж 2",
+		connectedTo: [1,27]
 	},
 	{
-		"x": 897,
-		"y": 2967,
-		"name": "Главный корпус",
-		"from": "mosfilm-2-3",
-		"to": "mosfilm-1"
+		"id": 17,
+		"x": 940,
+		"y": 589,
+		"name": "Лифт",
+		"from": "mgu-2",
+		title: "Этаж 2",
+		connectedTo: [5,29]
 	},
 	{
-		"x": 286,
-		"y": 1398,
-		"name": "Главный корпус",
-		"from": "mosfilm-5-3",
-		"to": "mosfilm-1"
+		"id": 19,
+		"x": 1156,
+		"y": 589,
+		"name": "Лифт",
+		"from": "mgu-2",
+		title: "Этаж 2",
+		connectedTo: [7,31]
 	},
 	{
-		"x": 725,
-		"y": 110,
-		"name": "Главный корпус",
-		"from": "mosfilm-6-1",
-		"to": "mosfilm-1"
+		"id": 21,
+		"x": 940,
+		"y": 957,
+		"name": "Лифт",
+		"from": "mgu-2",
+		title: "Этаж 2",
+		connectedTo: [9,33]
 	},
 	{
-		"x": 808,
-		"y": 207,
-		"name": "Этаж 2",
-		"from": "mosfilm-6-1",
-		"to": "mosfilm-6-2"
+		"id": 23,
+		"x": 1155,
+		"y": 957,
+		"name": "Лифт",
+		"from": "mgu-2",
+		title: "Этаж 2",
+		connectedTo: [11,35]
 	},
 	{
-		"x": 810,
-		"y": 206,
-		"name": "Этаж 1",
-		"from": "mosfilm-6-2",
-		"to": "mosfilm-6-1"
+		"id": 25,
+		"x": 280,
+		"y": 493,
+		"name": "Лифт",
+		"from": "mgu-3",
+		title: "Этаж 3",
+		connectedTo: [3,13]
 	},
 	{
-		"x": 94,
-		"y": 268,
-		"name": "Строение 5, этаж 3",
-		"from": "mosfilm-6-2",
-		"to": "mosfilm-5-3"
-	},
-	{
-		"x": 38,
+		"id": 27,
+		"x": 280,
 		"y": 376,
-		"name": "Главный корпус",
-		"from": "mosfilm-7-1",
-		"to": "mosfilm-1"
+		"name": "Лифт",
+		"from": "mgu-3",
+		title: "Этаж 3",
+		connectedTo: [1,15]
 	},
 	{
-		"x": 926,
-		"y": 577,
-		"name": "Этаж 2",
-		"from": "mosfilm-7-1",
-		"to": "mosfilm-7-2"
+		"id": 29,
+		"x": 940,
+		"y": 589,
+		"name": "Лифт",
+		"from": "mgu-3",
+		title: "Этаж 3",
+		connectedTo: [5,17]
 	},
 	{
-		"x": 1260,
-		"y": 761,
-		"name": "Этаж 1",
-		"from": "mosfilm-7-2",
-		"to": "mosfilm-7-1"
+		"id": 31,
+		"x": 1155,
+		"y": 589,
+		"name": "Лифт",
+		"from": "mgu-3",
+		title: "Этаж 3",
+		connectedTo: [7,19]
 	},
 	{
-		"x": 580,
-		"y": 144,
-		"name": "Главный корпус",
-		"from": "mosfilm-18-1",
-		"to": "mosfilm-1"
+		"id": 33,
+		"x": 940,
+		"y": 957,
+		"name": "Лифт",
+		"from": "mgu-3",
+		title: "Этаж 3",
+		connectedTo: [9,21]
 	},
 	{
-		"x": 557,
-		"y": 220,
-		"name": "Этаж 2",
-		"from": "mosfilm-18-1",
-		"to": "mosfilm-18-2"
-	},
-	{
-		"x": 504,
-		"y": 220,
-		"name": "Этаж 1",
-		"from": "mosfilm-18-2",
-		"to": "mosfilm-18-1"
-	},
-	{
-		"x": 695,
-		"y": 661,
-		"name": "Главный корпус",
-		"from": "mosfilm-27-1",
-		"to": "mosfilm-1"
-	},
-	{
-		"x": 619,
-		"y": 649,
-		"name": "Этаж 2",
-		"from": "mosfilm-27-1",
-		"to": "mosfilm-27-2"
-	},
-	{
-		"x": 600,
-		"y": 650,
-		"name": "Этаж 1",
-		"from": "mosfilm-27-2",
-		"to": "mosfilm-27-1"
-	},
-	{
-		"x": 745,
-		"y": 1092,
-		"name": "Главный корпус",
-		"from": "mosfilm-3-1",
-		"to": "mosfilm-1"
-	},
-	{
-		"x": 462,
-		"y": 106,
-		"name": "Главный корпус",
-		"from": "mosfilm-20-1",
-		"to": "mosfilm-1"
+		"id": 35,
+		"x": 1155,
+		"y": 957,
+		"name": "Лифт",
+		"from": "mgu-3",
+		title: "Этаж 3",
+		connectedTo: [11,23]
 	}
 ];
--- MarketGid.UI/Scripts/maps.js
+++ MarketGid.UI/Scripts/maps.js
@@ -1,54 +1,14 @@
 ﻿var MapsData = [
   {
-    "name": "mosfilm-1",
-    "backgroundImage": "/Content/maps/map050.jpg"
-  },
-  {
-    "name": "mosfilm-2-3",
+    "name": "mgu-1",
     "backgroundImage": null
   },
   {
-    "name": "mosfilm-5-3",
+    "name": "mgu-2",
     "backgroundImage": null
   },
   {
-    "name": "mosfilm-6-1",
-    "backgroundImage": null
-  },
-  {
-    "name": "mosfilm-6-2",
-    "backgroundImage": null
-  },
-  {
-    "name": "mosfilm-7-1",
-    "backgroundImage": null
-  },
-  {
-    "name": "mosfilm-7-2",
-    "backgroundImage": null
-  },
-  {
-    "name": "mosfilm-18-1",
-    "backgroundImage": null
-  },
-  {
-    "name": "mosfilm-18-2",
-    "backgroundImage": null
-  },
-  {
-    "name": "mosfilm-27-1",
-    "backgroundImage": null
-  },
-  {
-    "name": "mosfilm-27-2",
-    "backgroundImage": null
-  },
-  {
-    "name": "mosfilm-20-1",
-    "backgroundImage": null
-  },
-  {
-    "name": "mosfilm-3-1",
+    "name": "mgu-3",
     "backgroundImage": null
   }
 ];
