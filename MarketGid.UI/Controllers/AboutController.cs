5fe811375fb300a2e87cfd02dee5e033e6dc3614
nixxa <nixxa@mail.ru>
2013-11-20 12:02:54 +0400

    Data

---
+++ MarketGid.UI/Controllers/AboutController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MarketGid.Core;
+
+namespace MarketGid.UI.Controllers
+{
+	/// <summary>
+	/// Контроллер для разной ифформации
+	/// </summary>
+    public class AboutController : BaseController
+    {
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MarketGid.UI.Controllers.AboutController"/> class.
+		/// </summary>
+		/// <param name="factory">Factory.</param>
+		public AboutController(IUnitOfWorkFactory factory) : base(factory)
+		{
+		}
+
+		/// <summary>
+		/// Раздел информации об МГУ
+		/// </summary>
+        public ActionResult Info()
+        {
+            return View ();
+        }
+
+		/// <summary>
+		/// Раздел для поступающих
+		/// </summary>
+		public ActionResult Abiturient()
+		{
+			return View ();
+		}
+    }
+}
