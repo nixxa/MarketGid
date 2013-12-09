5463a5343a1aa6076eaff30adf2d51b96c993a78
nixxa <nixxa@mail.ru>
2013-12-09 12:56:34 +0400

    Data

--- startup.ps1
+++ startup.ps1
@@ -1,8 +1,10 @@
 param([Int32]$kiosk=1)
 $webServer = "c:\program files\iis express\iisexpress.exe"
 $webServerParams = "/path:C:\SkyDrive\marketgid /port:8080"
-$webBrowser = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
-$webBrowserParams = "-kiosk http://localhost:8080/?kiosk=" + $kiosk
+#$webBrowser = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+#$webBrowserParams = "-kiosk http://localhost:8080/?kiosk=" + $kiosk
+$webBrowser = "C:\Program Files\Internet Explorer\iexplore.exe"
+$webBrowserParams = "-k http://localhost:8080/?kiosk=" + $kiosk
 $target = "iisexpress"
 
 $process = Get-Process | Where-Object {$_.ProcessName -eq $target}
